b1e53ccd347a9b6520313443186dfd8b19cba5a6
Claire Robson <claire.robson1@barclays.com>
2024-10-04 11:04:39 +0100

    Scooter update

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,38 @@
 class Scooter {
-  // scooter code here
+
+ static nextSerial = 1;
+
+ constructor(station){
+  this.station = station;
+  this.user = null;
+  this.serial = Scooter.nextSerial++;
+  this.isBroken = false;
+  this.charge = 100;
+
+ }
+
+ rent(user) {
+  if (this.charge < 20){
+    throw new Error ("Scooter needs to charge")
+  } else if (this.isBroken){
+    throw new Error ("Scooter needs repair")
+  } else {
+    this.user = user;
+    this.station = null;
+    return this.serial;
+
+  }
+  
+  }
+dock(station) {
+  this.station = station;
+  this.user = null;
 }
+
+ }
+
+
+
+
 
 module.exports = Scooter
--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
 class User {
-  // User code here
+ constructor(username, age, password){
+  this.username = username;
+  this.age = age;
+  this.password = password; 
+  this.loggedIn = false;
+ }
+
+login(password) {
+  if (this.password === password){
+    this.loggedIn = true
+  } else { 
+    throw new Error ("Incorrect password")
+}
+}
+
+logout() {
+  this.loggedIn = false;
+}
+
 }
 
 module.exports = User
